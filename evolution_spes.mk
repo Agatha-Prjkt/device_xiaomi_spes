6d2540b2ea4ed2be7c17649cb5a670f5057b28c4
Agatha q <naufalmarjan2014@gmail.com>
2023-03-12 18:25:38 +0000

    spes: Evolution X time!
    * Added Smart Charging

--- evolution_spes.mk
+++ evolution_spes.mk
@@ -8,13 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-PRODUCT_NAME := lineage_spes
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+# Evolution-X Stuff
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_ENABLE_BLUR := true
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NAME := evolution_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
