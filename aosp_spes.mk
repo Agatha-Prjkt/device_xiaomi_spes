3afa6992e6b2870a7e9e7dd385afc1f16774edfe
Agatha q <naufalmarjan2014@gmail.com>
2023-03-13 13:37:17 +0000

    spes: Pixel Extended bringup

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,20 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
-
-# Evolution-X Stuff
-EVO_BUILD_TYPE := UNOFFICIAL
-TARGET_ENABLE_BLUR := true
-
-# Boot Animation Resolution
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_GAPPS_ARCH := arm64
+FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
 
-PRODUCT_NAME := evolution_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
